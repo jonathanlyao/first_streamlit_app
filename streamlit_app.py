9b7e2d6cc9f663e1d12f9e06ebb5a47dc8c24ef7
Lee Yao <104183693+jonathanlyao@users.noreply.github.com>
2023-03-03 03:44:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
-import snowflake.connector
+# import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
